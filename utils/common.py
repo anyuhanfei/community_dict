356e28d2153d6328ad80bbd8caeba4dd34172af0
许寒飞 <xuhanfei@192.168.0.39>
2020-10-01 12:21:08 +0800

    吉屋网采集及简单测试 and 打印并休眠函数修改

--- utils/common.py
+++ utils/common.py
@@ -3,8 +3,8 @@ import time
 from config.config import time_interval
 
 
-def print_and_sleep(*args, **kwargs):
+def print_and_sleep(*args, time_interval=time_interval):
     '''打印内容后休眠一定时间
     '''
-    print(*args, **kwargs)
+    print(*args)
     time.sleep(time_interval)
